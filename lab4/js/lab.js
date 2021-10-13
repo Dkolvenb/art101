06de0d6747de2380cea9df83ab949dbbb821d155
Dkolvenb <dkolvenb@ucsc.edu>
2021-10-12 19:53:24 -0700

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,26 @@
+/**
+ * Pseudocode for an Automated Program boot-up.
+ *
+ * An automated program that allows the user to select times and dates at which they would like specific programs to be started upon computer boot-up.
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author Ben Putaski and Dylan Kolvenbach
+ * @since  8.12.2021
+ */
+
+/** jshint {inline configuration here} */
+
+//Automated Program boot-up through calendar set-up
+  //prompt user for time and dates of boot-up
+    //Check that it is a valid time and date
+      //if yes continue
+      //if no repeat prompt
+  //Have user select programs to be started at designated times and dates
+    //Check that the programs are installed on the computer
+      //if yes continue
+      //if no repeat prompt
+  //Generate pop-up message for confirmation by user
+  //Check current date and time
+    //repeat until matching the user input
+  //start up selected programs
