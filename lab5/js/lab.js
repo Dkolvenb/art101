47b2e8dcf684c486b8372f642adb9ac1bad5ad2d
Dkolvenb <dkolvenb@ucsc.edu>
2021-10-17 18:18:57 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Dylan Kolvenbach <dkolvenb@ucsc.edu>
+ * Created: 17 October 2021
+ * License: Public Domain
+ */
+
+ // Define Variables
+ make = "Chevrolet";
+ model = "Corvete Stingray";
+ color = "Red";
+ year = 1967;
+
+ // calculate
+ age = 2021 - year;
+
+ // output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
