f1a9314a56f60071558997f133760f77eef31acc
Dkolvenb <dkolvenb@ucsc.edu>
2021-10-24 11:17:05 -0700

    Lab 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -6,7 +6,7 @@
 
 // sortUserName - a function that takes user input and sorts the letters of their name
 function sortUserName() {
-  var userName = window.prompt("Hi, Please tell me your name so I can fix it.");
+  var userName = window.prompt("Hi, Please tell me your full name so I can fix it.");
   console.log("userName =", userName);
   //split string to array
   var nameArray = userName.split('');
@@ -19,6 +19,7 @@ function sortUserName() {
   console.log("nameSorted =", nameSorted);
   return nameSorted;
 }
+
 
 // output
 document.writeln("Oh hey, I've fixed your name: ", sortUserName(), "<br>");
