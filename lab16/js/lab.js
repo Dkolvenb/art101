c02610fb9be70db5b812f9b5033ced29e786f1af
Dkolvenb <dkolvenb@ucsc.edu>
2021-12-01 13:42:04 -0800

    Lab 16

---
+++ lab16/js/lab.js
@@ -0,0 +1,39 @@
+/*
+ * Author: Dylan Kolvenbach <dkolvenb@ucsc.edu>
+ * Created: 1 December 2021
+ * License: Public Domain
+ */
+
+var myUrl = "https://xkcd.com/info.0.json";
+
+$("#activate").click(runAPI);
+
+function printOutput(text){
+  $("#output").html(text);
+};
+
+function runAPI() {
+  console.log("click");
+  // Using the core $.ajax() method
+  $.ajax({
+    // API endpoint
+    url: myUrl,
+    // POST or GET request
+    type: "GET",
+    // data type we expect back
+    dataType : "json",
+})
+// If the request succeeds
+// data is passed back
+.done(function(data) {
+    console.log("Success:", data);
+    var imgUrl = data.img;
+    var imgTag = "<img src=" + imgUrl + ">";
+    console.log(imgUrl, imgTag);
+    $("#output").html(imgTag)
+})
+// If the request fails
+.fail(function(request,error) {
+    console.log(request, error);
+});
+}
